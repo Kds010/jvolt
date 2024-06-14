672318ce91d605a9210fa9fac574c56510f30263
Kds010 <rkfdktkeo2@naver.com>
2024-06-14 22:04:22 +0900

    Update UserMapper.java

--- jvolt/src/main/java/com/ho/jvolt/user/mapper/UserMapper.java
+++ jvolt/src/main/java/com/ho/jvolt/user/mapper/UserMapper.java
@@ -12,10 +12,6 @@ import org.mapstruct.factory.Mappers;
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "firstname", target = "firstname")
-    @Mapping(source = "lastname", target = "lastname")
-    @Mapping(source = "email", target = "email")
     @Mapping(target = "success", ignore = true) // Custom fields
     @Mapping(target = "message", ignore = true) // Custom fields
     RegisterResponse userToRegisterResponse(User user);
