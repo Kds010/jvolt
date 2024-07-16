9cee4370b1d11d8b4a56668ce8ed70e9670300c6
Kds010 <rkfdktkeo2@naver.com>
2024-07-16 19:52:16 +0900

    jwt secret key hiding

--- jvolt/src/main/java/com/ho/jvolt/common/security/config/JwtService.java
+++ jvolt/src/main/java/com/ho/jvolt/common/security/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "b619670a04dc1ec72620901bb6cfa8adbad690b73da213721a5042aaa02e0d6a";
+    @Value("${jwt.secret_key}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
